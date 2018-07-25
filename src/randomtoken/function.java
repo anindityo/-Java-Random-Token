f861e79024bef69e46374883c00225f3126dad90
ALDO <ALDO@DESKTOP-CO18VF5>
2018-07-25 11:07:31 +0700

    Menambahkan komen data array

---
+++ src/randomtoken/function.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package randomtoken;
+
+import java.util.Random;
+
+/**
+ *
+ * @author Gusma
+ */
+public class function {
+
+    /**
+     * Fungsi dengan menggunakan data tipe char diconvert menjadi String
+     * Dengan fungsi Random untuk menghasilkan data acak dari data array.
+     * Fungsi menampilkan data Array dengan hasil Token (Acak). dengan non void
+     * @param panjang integer
+     * @return String baru berisi (charbaru)
+     */
+
+    public String token(int panjang) {
+
+        char[] isi = "0123456789ABCDEFGHJIKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".toCharArray(); // Data Array
+        char[] charbaru = new char[panjang];
+        Random acak = new Random(); 
+        for (int i = 0; i < panjang; i++) { 
+            charbaru[i] = isi[acak.nextInt(isi.length)];
+        }
+        return new String(charbaru);
+    }
+}
